879e3ba13a1ea07008da1b19a237a99dd4ff1240
Lin Liu <liulin0096@gmail.com>
2025-06-05 00:39:04 +0300

    child1\2

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lin <lin@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 11:33:35 by linliu            #+#    #+#             */
-/*   Updated: 2025/06/04 18:27:53 by lin              ###   ########.fr       */
+/*   Updated: 2025/06/04 22:58:30 by lin              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,55 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    int     infile;
-    int     outfile;
+    int pipefd[2];
     pid_t   pid[2];
     
     if(argc != 5)
         return(1);
-    infile = open(argv[1], O_RDONLY);
-    outfile = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if(infile == -1 || outfile == -1)
+    if(pipefd < 0)
     {
         
     }
+    outfile = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0664);
     
-    pid[0] = child1_process(argv, infile, 1, envp);
+    pid[0] = child1_process(argv, pipefd, envp);
     pid[1] = child2_process(argv, outfile, 0, envp);
     waitpid(pid[0], NULL, 0);
     waitpid(pid[1], NULL, 0); 
     return(0);
 }
 
+int    child1_process(char **argv, int *pipefd, char **envp)
+{
+    pid_t pid;
+    int infile;
+    char **cmd_args = split_cmd(argv[2], envp);
+    char *cmd_path = get_cmd_path(cmd_args[0], envp);
+
+    infile = open(argv[1], O_RDONLY);
+    if (infile < 0 )
+        error_exit("open file");
+    pid = fork();
+    if(pid < 0)
+        error_exit("fork");
+    if (pid == 0)
+    {
+        dup2(infile, STDIN_FILENO);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(infile);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execve(cmd_path, cmd_args, envp);
+        error_exit("exeve");
+    }
+    return (pid);
+}
+
+void    error_exit(const char *str)
+{
+    perror(str);
+    exit(1);
+}
 //-------------------------------------------------------
 //get_cmd_path and check if it can run
 char *get_cmd_path(char *cmd, char **envp)
@@ -94,30 +123,4 @@ char **split_cmd(char *cmd, char **envp)
         return (NULL);
 }
 
-int    child1_process(char **argv, int file_fd, int pipe_nub, char **envp)
-{
-    int pipefd[2];
-    pid_t pid;
-    char **cmd_args = split_cmd(argv[2], envp);
-    char *cmd_path = get_cmd_path(cmd_args[0], envp);
 
-    if(pipe(pipefd) < 0)
-    {
-        
-    }
-    pid = fok();
-    if(pid < 0)
-    {
-       
-        
-    }
-    if (pid == 0)
-    {
-        dup2(file_fd, STDIN_FILENO);
-        dup2(pipefd[pipe_nub], STDOUT_FILENO);
-        close(file_fd);
-        close(pipefd[pipe_nub]);
-        execve(cmd_path, cmd_args, envp);
-    }
-    return (pid);
-}
